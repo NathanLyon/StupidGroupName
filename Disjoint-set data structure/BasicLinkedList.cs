723ca9e5020b4a0e243f636606429aba1ff514f7
Paul Baker Le Salt Shaker <son.of.lysander@gmail.com>
2013-11-20 12:29:30 -0700

    initial commit

---
+++ Disjoint-set data structure/BasicLinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dsjnt01
+{
+    class BasicLinkedList
+    {
+        class Node<T>
+        {
+            public 
+        }
+    }
+}
