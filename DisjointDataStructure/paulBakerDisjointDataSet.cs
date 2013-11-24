51115290b5d219f1766e151692496bff05d627e6
Paul Baker <son.of.lysander@gmail.com>
2013-11-23 18:39:00 -0700

    Fixed a minor bug in 'MakeSet'. Slightly Optimized Union. For our intents and purposes I believe that my part is essentially done.

--- DisjointDataStructure/paulBakerDisjointDataSet.cs
+++ DisjointDataStructure/paulBakerDisjointDataSet.cs
@@ -12,7 +12,7 @@ namespace paulbaker
         //I went this direction because I was short on time, but it was simple and made sense to me. It's based off the descripting of "Quick Find" in this
         //youtube video. http://www.youtube.com/watch?v=gcmjC-OcWpI
 
-        Dictionary<T, HashSet<T>> universe;
+        public Dictionary<T, HashSet<T>> universe; //this is public for testing purposes only. Normally this will be private. I might consider making this class enumerable later.
 
         /// <summary>
         /// Create a Disjoint Data Set
@@ -29,13 +29,14 @@ namespace paulbaker
         /// <returns>Will return false if this item is already a member of a set, otherwise returns true indicating a new set has been created.</returns>
         public bool MakeSet(T item)
         {
-            bool add = universe.ContainsKey(item);
-            if (!add)
+            if (universe.ContainsKey(item)) //if the item already exists, then we cannot add it to our set again.
             {
-                universe.Add(item, new HashSet<T>());
-                universe[item].Add(item);
+                return false;
             }
-            return add;
+            //the item doesn't exist, ergo we are clear to add it and initialize it's own set.
+            universe.Add(item, new HashSet<T>());
+            universe[item].Add(item);
+            return true;
         }
 
         /// <summary>
@@ -63,19 +64,37 @@ namespace paulbaker
         {
             HashSet<T> setA = Find(a);
             HashSet<T> setB = Find(b);
-            if (setA == null || setB == null)
+            if (setA == null || setB == null) // if one of the elements doens't exist in our universe.
             {
                 return null;
             }
-            else if (System.Object.ReferenceEquals(setA, setB))
+            else if (System.Object.ReferenceEquals(setA, setB)) // if both of the elements are in the same set (or even same item) return the set. No union is needed.
             {
                 return setA;
             }
-            HashSet<T> result = new HashSet<T>();
+            /*HashSet<T> result = new HashSet<T>(); //it is suboptimal to make a new set and add both  to it instead of adding the smaller set to the larger.     Will change this later when I have time. 
             result.UnionWith(setA);
             result.UnionWith(setB);
             //as the video described, this will unfortunately be in linear time...
             foreach (T item in result)
+            {
+                universe[item] = result;
+            }
+            return result;*/
+            HashSet<T> result;
+            HashSet<T> lesser;
+            if (setA.Count < setB.Count)
+            {
+                lesser = setA;
+                result = setB;
+            }
+            else
+            {
+                lesser = setB;
+                result = setA;
+            }
+            result.UnionWith(lesser);
+            foreach (T item in lesser)
             {
                 universe[item] = result;
             }
